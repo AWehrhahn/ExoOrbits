ce919e8162649090a510db9b16ed66fc8908af61
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-07-24 13:21:43 +0200

    Add library of example objects, e.g. Sun and Earth

--- example/example.py
+++ example/example.py
@@ -3,43 +3,43 @@ import matplotlib.pyplot as plt
 from astropy import constants as const
 from astropy import units as q
 
-from ExoOrbit import Orbit
+from ExoOrbit import Orbit, Body
+from ExoOrbit.library import Sun, Earth, Earth_circular
 
 if __name__ == "__main__":
-    a = q.AU.to("km")
+    # Earth parameters
     p = q.year.to("day")
-    e = 0.1
-    i = np.pi / 2
-    w = np.pi / 2 + 0.1
 
-    r_s = const.R_sun.to("km").value
-    m_s = const.M_sun.to("kg").value
-    t_s = 5770
-    r_p = const.R_earth.to("km").value
-    m_p = const.M_earth.to("kg").value
+    orbit = Orbit(Sun, Earth)
 
-    orbit = Orbit(a, p, e, i, w, r_s, m_s, t_s, r_p, m_p)
-
-    t = np.linspace(0, p, 1000)
+    t = np.linspace(-p/2, p/2, 100000)
 
     m = orbit.mean_anomaly(t)
     e = orbit.eccentric_anomaly(t)
     f = orbit.true_anomaly(t)
-    d = orbit.distance(t)
-    r = orbit.phase_angle(t)
+    d = orbit.transit_depth(t)
+    p = orbit.phase_angle(t)
+    r = orbit.projected_radius(t)
+    rv = orbit.radial_velocity_planet(t)
 
     t0 = orbit.time_primary_transit()
-    t0 = orbit.time_secondary_eclipse()
+    # t0 = orbit.time_secondary_eclipse()
     t1 = orbit.first_contact()
+    t2 = orbit.second_contact()
+    t3 = orbit.third_contact()
     t4 = orbit.fourth_contact()
     print(t1)
     print(t4)
 
-    plt.plot(t, r)
+    y = rv
+    plt.plot(t, y)
 
     # plt.hlines(r_s + r_p, 0, p, colors="r")
-    plt.vlines((t1, t4), 0, np.max(r), colors="r")
-    plt.vlines(t0, 0, np.max(r), colors="g")
+    plt.vlines((t1, t2, t3, t4), np.min(y), np.max(y), colors="r")
+    plt.vlines(t0, np.min(y), np.max(y), colors="g")
+
+    plt.xlabel("Day")
+    plt.ylabel("Radial velocity [m/s]")
 
     plt.show()
 
