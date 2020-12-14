45e7b555a747fa1549bfb9a05e9bc75df0d56fc1
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2020-12-14 16:34:17 +0100

    Versioneer

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
+import versioneer
 
-setup(name="exoorbit", packages=find_packages())
+setup(
+    name="exoorbit",
+    packages=find_packages(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
